0c790d9feacc459a7ed6219c91e2b8e4ee070592
meli197 <melina.-97@hotmail.es>
2021-12-06 15:14:31 -0300

    for

--- main.js
+++ main.js
@@ -17,12 +17,18 @@ let torta5 = 12;
 let torta6 = 5;
 let torta7 = 25;
 
+
+
+
 let cantidad = prompt("Hola" +" "+ nombreIngresado + " " + "por favor ingrese la cantidad de productos que desea.");
+for(let torta1 = 1; torta1 < 150; torta1++) {
+    
+if (torta1 == 15) {
+   break;
+}
 
+alert ("¡Perfecto!" + " " +nombreIngresado +" "+" Ahora para finalizar le pedimos que complete el formulario."+ " " + "Le informamos que el stock disponible es" +" "+torta1)
+}
 let stock = torta1 - cantidad;
-
-alert ("¡Perfecto! Ahora para finalizar le pedimos que complete el formulario.")
-
-
 
 
